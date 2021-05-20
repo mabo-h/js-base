aa78211dbfef0da2ede3bc79c42f18fdce90c9ff
mabo <maboro@mail.ru>
2021-05-20 16:36:58 +0700

    Основное задание (1 и 2) по первому уроку

---
+++ lesson 1/js/script.js
@@ -0,0 +1,11 @@
+alert('Задание 1');
+var tempC = +prompt('Введите температуру по Цельсию', '');
+var tempF = (9 / 5) * tempC + 32;
+alert('Температура по Форенгейту: ' + tempF);
+
+alert('Задание 2');
+let admin = '';
+let name = 'Василий';
+admin = name;
+alert(admin);
+
